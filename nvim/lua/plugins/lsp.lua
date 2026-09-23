ffa259b3acc25ee2101d9fbed7e76e17561892ae
TheRealBYK <byknintendo@gmail.com>
2026-09-23 18:35:59 +0300

    Fixed errors that might show up due to miscofinguration.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,250 +1,252 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        -- Autocompletion
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        -- Snippets
-        "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
-    },
-    config = function()
-        local autoformat_filetypes = {
-            "lua",
-            "py",
-            "jar",
-            "c",
-            "cpp",
-            "cs",
-            "css",
-            "toml",
-            "yaml",
-            "xml",
-            "html",
-            "dart",
-        }
-        -- Create a keymap for vim.lsp.buf.implementation
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(args)
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if not client then
-                    return
-                end
-                if vim.tbl_contains(autoformat_filetypes, vim.bo.filetype) then
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        buffer = args.buf,
-                        callback = function()
-                            vim.lsp.buf.format({
-                                formatting_options = { tabSize = 4, insertSpaces = true },
-                                bufnr = args.buf,
-                                id = client.id,
-                            })
-                        end,
-                    })
-                end
-            end,
-        })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		-- Snippets
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local autoformat_filetypes = {
+			"lua",
+			"py",
+			"jar",
+			"c",
+			"cpp",
+			"cs",
+			"css",
+			"toml",
+			"yaml",
+			"xml",
+			"html",
+			"dart",
+		}
+		-- Create a keymap for vim.lsp.buf.implementation
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client then
+					return
+				end
+				if vim.tbl_contains(autoformat_filetypes, vim.bo.filetype) then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({
+								formatting_options = { tabSize = 4, insertSpaces = true },
+								bufnr = args.buf,
+								id = client.id,
+							})
+						end,
+					})
+				end
+			end,
+		})
 
-        -- Configure error/warnings interface
-        vim.diagnostic.config({
-            virtual_text = true,
-            severity_sort = true,
-            float = {
-                style = "minimal",
-                border = "rounded",
-                header = "",
-                prefix = "",
-            },
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "✘",
-                    [vim.diagnostic.severity.WARN] = "▲",
-                    [vim.diagnostic.severity.HINT] = "⚑",
-                    [vim.diagnostic.severity.INFO] = "»",
-                },
-            },
-        })
+		-- Configure error/warnings interface
+		vim.diagnostic.config({
+			virtual_text = true,
+			severity_sort = true,
+			float = {
+				style = "minimal",
+				border = "rounded",
+				header = "",
+				prefix = "",
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "✘",
+					[vim.diagnostic.severity.WARN] = "▲",
+					[vim.diagnostic.severity.HINT] = "⚑",
+					[vim.diagnostic.severity.INFO] = "»",
+				},
+			},
+		})
 
-        -- Add cmp_nvim_lsp capabilities settings to lspconfig
-        -- This should be executed before you configure any language server
-        local status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-        if status then
-            vim.lsp.config("*", {
-                capabilities = cmp_nvim_lsp.default_capabilities(),
-            })
-        end
+		-- Add cmp_nvim_lsp capabilities settings to lspconfig
+		-- This should be executed before you configure any language server
+		local status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+		if status then
+			vim.lsp.config("*", {
+				capabilities = cmp_nvim_lsp.default_capabilities(),
+			})
+		end
 
-        -- This is where you enable features that only work
-        -- if there is a language server active in the file
-        vim.api.nvim_create_autocmd("LspAttach", {
-            callback = function(event)
-                local opts = { buffer = event.buf }
+		-- This is where you enable features that only work
+		-- if there is a language server active in the file
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(event)
+				local opts = { buffer = event.buf }
 
-                vim.keymap.set("n", "K", function()
-                    vim.lsp.buf.hover({ borsder = "rounded" })
-                end, opts)
-                vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-                vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-                vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-                vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-                vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-                vim.keymap.set("n", "gs", function()
-                    vim.lsp.buf.signature_help({ borsder = "rounded" })
-                end, opts)
-                vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-                vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-                vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-                vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-            end,
-        })
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover({ borsder = "rounded" })
+				end, opts)
+				vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+				vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+				vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+				vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+				vim.keymap.set("n", "gs", function()
+					vim.lsp.buf.signature_help({ borsder = "rounded" })
+				end, opts)
+				vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+				vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+				vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+				vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+			end,
+		})
 
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "intelephense",
-                "ts_ls",
-                "eslint",
-            },
-            handlers = {
-                -- this first function is the "default handler"
-                -- it applies to every language server without a custom handler
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"intelephense",
+				"ts_ls",
+				"eslint",
+			},
+			handlers = {
+				-- this first function is the "default handler"
+				-- it applies to every language server without a custom handler
+				function(server_name)
+					if server_name ~= "snyk_ls" then
+						require("lspconfig")[server_name].setup({})
+					end
+				end,
 
-                -- this is the "custom handler" for `lua_ls`
-                lua_ls = function()
-                    require("lspconfig").lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                runtime = {
-                                    version = "LuaJIT",
-                                },
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                workspace = {
-                                    library = { vim.env.VIMRUNTIME },
-                                },
-                            },
-                        },
-                    })
-                end,
-            },
-        })
+				-- this is the "custom handler" for `lua_ls`
+				lua_ls = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = { vim.env.VIMRUNTIME },
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
 
-        local cmp = require("cmp")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local cmp = require("cmp")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        vim.lsp.config("clangd", {
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--background-index", -- Index project in the background
-                "--clang-tidy", -- Enable linting
-                "--header-insertion=iwyu", -- "Include What You Use" (auto-includes headers)
-                -- "--compile-commands-dir=../build",
-                "--fallback-style=none",
-            },
-        })
+		vim.lsp.config("clangd", {
+			capabilities = capabilities,
+			cmd = {
+				"clangd",
+				"--background-index", -- Index project in the background
+				"--clang-tidy", -- Enable linting
+				"--header-insertion=iwyu", -- "Include What You Use" (auto-includes headers)
+				-- "--compile-commands-dir=../build",
+				"--fallback-style=none",
+			},
+		})
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+		vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-        cmp.setup({
-            preselect = "item",
-            completion = {
-                completeopt = "menu,menuone,noinsert",
-            },
-            window = {
-                documentation = cmp.config.window.bordered(),
-            },
-            sources = {
-                { name = "path" },
-                { name = "nvim_lsp" },
-                { name = "buffer",  keyword_length = 3 },
-                { name = "luasnip", keyword_length = 2 },
-            },
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            formatting = {
-                fields = { "abbr", "menu", "kind" },
-                format = function(entry, item)
-                    local n = entry.source.name
-                    if n == "nvim_lsp" then
-                        item.menu = "[LSP]"
-                    else
-                        item.menu = string.format("[%s]", n)
-                    end
-                    return item
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                -- confirm completion item
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+		cmp.setup({
+			preselect = "item",
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			window = {
+				documentation = cmp.config.window.bordered(),
+			},
+			sources = {
+				{ name = "path" },
+				{ name = "nvim_lsp" },
+				{ name = "buffer", keyword_length = 3 },
+				{ name = "luasnip", keyword_length = 2 },
+			},
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			formatting = {
+				fields = { "abbr", "menu", "kind" },
+				format = function(entry, item)
+					local n = entry.source.name
+					if n == "nvim_lsp" then
+						item.menu = "[LSP]"
+					else
+						item.menu = string.format("[%s]", n)
+					end
+					return item
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				-- confirm completion item
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-                -- scroll documentation window
-                ["<C-f>"] = cmp.mapping.scroll_docs(5),
-                ["<C-u>"] = cmp.mapping.scroll_docs(-5),
+				-- scroll documentation window
+				["<C-f>"] = cmp.mapping.scroll_docs(5),
+				["<C-u>"] = cmp.mapping.scroll_docs(-5),
 
-                -- toggle completion menu
-                ["<C-e>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.abort()
-                    else
-                        cmp.complete()
-                    end
-                end),
+				-- toggle completion menu
+				["<C-e>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.abort()
+					else
+						cmp.complete()
+					end
+				end),
 
-                -- tab complete
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    local col = vim.fn.col(".") - 1
+				-- tab complete
+				["<Tab>"] = cmp.mapping(function(fallback)
+					local col = vim.fn.col(".") - 1
 
-                    if cmp.visible() then
-                        cmp.select_next_item({ behavior = "select" })
-                    elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-                        fallback()
-                    else
-                        cmp.complete()
-                    end
-                end, { "i", "s" }),
+					if cmp.visible() then
+						cmp.select_next_item({ behavior = "select" })
+					elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+						fallback()
+					else
+						cmp.complete()
+					end
+				end, { "i", "s" }),
 
-                -- go to previous item
-                ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+				-- go to previous item
+				["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
 
-                -- navigate to next snippet placeholder
-                ["<C-d>"] = cmp.mapping(function(fallback)
-                    local luasnip = require("luasnip")
+				-- navigate to next snippet placeholder
+				["<C-d>"] = cmp.mapping(function(fallback)
+					local luasnip = require("luasnip")
 
-                    if luasnip.jumpable(1) then
-                        luasnip.jump(1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+					if luasnip.jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 
-                -- navigate to the previous snippet placeholder
-                ["<C-b>"] = cmp.mapping(function(fallback)
-                    local luasnip = require("luasnip")
+				-- navigate to the previous snippet placeholder
+				["<C-b>"] = cmp.mapping(function(fallback)
+					local luasnip = require("luasnip")
 
-                    if luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            }),
-        })
-    end,
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+		})
+	end,
 }
